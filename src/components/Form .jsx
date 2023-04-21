8ba3b39dd123be9bf366e2e0a08b21e6466c0277
taku95 <t.y.npng1995@gmail.com>
2023-04-21 17:26:36 +0900

    Feature created form and rendering submitted data

--- src/components/Form .jsx
+++ src/components/Form .jsx
@@ -1,44 +1,90 @@
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
-import React from "react";
+import React, { useState } from "react";
+import { Grid, TextField, Button, Typography } from "@material-ui/core";
 
 export const Form = () => {
   const {
-    register,
+    control,
     handleSubmit,
     reset,
     formState: { errors },
   } = useForm({
     defaultValues: {
-      name: "寺田心",
-      email: "bookOffnanoniHonNeJan@gmail.com",
+      name: "",
+      email: "",
     },
   });
 
+  const [submittedData, setSubmittedData] = useState(null);
+
   const onSubmit = (data) => {
     console.log(data);
+    setSubmittedData(data);
     reset();
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("name", { required: true, maxLength: 60 })} />
-      {errors.name && "名前を入力してください"}
-      <ErrorMessage errors={errors} name="name" />
-      <input
-        {...register("email", {
-          required: true,
-          maxLength: 60,
-          pattern: {
-            value:
-              /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-            message: "メールアドレスの形式が不正です",
-          },
-        })}
-      />
-      {errors.email && "メールアドレスを入力してください"}
-      <ErrorMessage errors={errors} name="email" />
-      <button type="submit">Submit</button>
-    </form>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Controller
+          control={control}
+          name="name"
+          rules={{ required: true }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Name"
+              error={!!errors.name}
+              helperText={errors.name && "Name is required"}
+              fullWidth
+            />
+          )}
+        />
+        <ErrorMessage errors={errors} name="name" />
+      </Grid>
+      <Grid item xs={12}>
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: true,
+            pattern: {
+              // eslint-disable-next-line no-useless-escape
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Invalid email address",
+            },
+          }}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              label="Email"
+              error={!!errors.email}
+              helperText={errors.email && "Please enter a valid email address"}
+              fullWidth
+            />
+          )}
+        />
+        <ErrorMessage errors={errors} name="email" />
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={handleSubmit(onSubmit)}
+        >
+          Submit
+        </Button>
+      </Grid>
+      {submittedData && (
+        <Grid item xs={12}>
+          <Typography variant="h5">Thanks {submittedData.name}</Typography>
+          <Typography variant="body1">
+            The email you submitted is: {submittedData.email}
+          </Typography>
+        </Grid>
+      )}
+    </Grid>
   );
 };
