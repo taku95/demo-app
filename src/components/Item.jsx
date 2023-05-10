551f76f454f03509b7f2a90902e2f0e5acd7196b
taku95 <t.y.npng1995@gmail.com>
2023-05-10 16:56:10 +0900

    Removed commented out code

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,47 +1,5 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-// /* eslint-disable no-unused-vars */
-// /* eslint-disable react/prop-types */
-// import React from "react";
-// import {
-//   Card,
-//   CardActionArea,
-//   CardContent,
-//   CardMedia,
-//   Typography,
-//   CardHeader,
-//   Box,
-// } from "@material-ui/core";
-
-// const Item = ({ item }) => {
-//   const removeLineBreaks = (description) => {
-//     if (description) {
-//       return description.replace(/<br>/g, "");
-//     }
-//   };
-
-//   return (
-//     <Card>
-//       <CardActionArea>
-//         <CardHeader title={item.name} />
-//         <Box sx={{ height: 300, width: 300 }}>
-//           <CardMedia component="img" image={item.exImage.url} alt={item.name} />
-//         </Box>
-
-//         <CardContent>
-//           {/* <Typography variant="body1" color="textSecondary" component="div">
-//             {removeLineBreaks(item.description)}
-//           </Typography> */}
-//           <Typography variant="h6" color="textSecondary" component="div">
-//             {item.price}円
-//           </Typography>
-//         </CardContent>
-//       </CardActionArea>
-//     </Card>
-//   );
-// };
-
-// export default Item;
 
 import React, { useState } from "react";
 import {
