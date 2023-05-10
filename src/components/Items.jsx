0828431407e1b83337f6fd9db9aecd9a41996a1b
taku95 <t.y.npng1995@gmail.com>
2023-05-10 14:52:52 +0900

    [WIP] pagenation 追加

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,18 +1,43 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from "react";
-// import { Link, Typography } from "@material-ui/core";
+import React, { useState } from "react";
 import { Stack } from "@mui/material";
+import MuiPagination from "@material-ui/lab/Pagination";
 import Item from "./Item";
 import Box from "@mui/material/Box";
 
 const Items = ({ searchResult }) => {
   const items = searchResult.hits ?? [];
-  // console.log(items);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const ITEMS_PER_PAGE = 10;
+  const totalPages = Math.ceil(items.length / ITEMS_PER_PAGE);
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+  const displayedItems = items.slice(startIndex, endIndex);
+
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page);
+  };
   return (
-    <Box sx={{ width: "60%" }}>
+    <Box
+      sx={{
+        width: "60%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Box>
+        <MuiPagination
+          count={totalPages}
+          page={currentPage}
+          onChange={handlePageChange}
+        />
+      </Box>
       <Stack direction="column" spacing={2}>
-        {items.map((item) => (
+        {displayedItems.map((item) => (
           <Item key={item.code} item={item}></Item>
         ))}
       </Stack>
