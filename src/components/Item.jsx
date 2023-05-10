418dc81171e4c6bdd55301c862847d2128072b2e
taku95 <t.y.npng1995@gmail.com>
2023-05-10 16:45:37 +0900

    [WPI] popover追加

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,40 +1,96 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React from "react";
+// /* eslint-disable no-unused-vars */
+// /* eslint-disable react/prop-types */
+// import React from "react";
+// import {
+//   Card,
+//   CardActionArea,
+//   CardContent,
+//   CardMedia,
+//   Typography,
+//   CardHeader,
+//   Box,
+// } from "@material-ui/core";
+
+// const Item = ({ item }) => {
+//   const removeLineBreaks = (description) => {
+//     if (description) {
+//       return description.replace(/<br>/g, "");
+//     }
+//   };
+
+//   return (
+//     <Card>
+//       <CardActionArea>
+//         <CardHeader title={item.name} />
+//         <Box sx={{ height: 300, width: 300 }}>
+//           <CardMedia component="img" image={item.exImage.url} alt={item.name} />
+//         </Box>
+
+//         <CardContent>
+//           {/* <Typography variant="body1" color="textSecondary" component="div">
+//             {removeLineBreaks(item.description)}
+//           </Typography> */}
+//           <Typography variant="h6" color="textSecondary" component="div">
+//             {item.price}円
+//           </Typography>
+//         </CardContent>
+//       </CardActionArea>
+//     </Card>
+//   );
+// };
+
+// export default Item;
+
+import React, { useState } from "react";
 import {
   Card,
   CardActionArea,
   CardContent,
   CardMedia,
   Typography,
-  CardHeader,
   Box,
 } from "@material-ui/core";
+import ItemDetailPopup from "./ItemDetailPopup";
 
 const Item = ({ item }) => {
-  const removeLineBreaks = (description) => {
-    if (description) {
-      return description.replace(/<br>/g, "");
-    }
+  const [selected, setSelected] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleCardClick = (event) => {
+    setSelected(!selected);
+    setAnchorEl(event.currentTarget);
   };
 
   return (
-    <Card>
+    <Card
+      sx={{
+        backgroundColor: selected ? "#eee" : "initial",
+        cursor: "pointer",
+      }}
+      onClick={handleCardClick}
+    >
       <CardActionArea>
-        <CardHeader title={item.name} />
-        <Box sx={{ height: 300, width: 300 }}>
+        <Box
+          display="flex"
+          justifyContent="center"
+          sx={{ height: 300, width: 300 }}
+        >
           <CardMedia component="img" image={item.exImage.url} alt={item.name} />
         </Box>
 
         <CardContent>
-          {/* <Typography variant="body1" color="textSecondary" component="div">
-            {removeLineBreaks(item.description)}
-          </Typography> */}
-          <Typography variant="h6" color="textSecondary" component="div">
+          <Typography gutterBottom variant="h5" component="div">
+            {item.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="div">
             {item.price}円
           </Typography>
         </CardContent>
       </CardActionArea>
+
+      <ItemDetailPopup item={item} open={selected} anchorEl={anchorEl} />
     </Card>
   );
 };
---
+++ src/components/ItemDetailPopup.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+import { Popover, Typography, Box } from "@material-ui/core";
+
+const ItemDetailPopup = ({ item, open, anchorEl }) => {
+  const removeLineBreaks = (description) => {
+    if (description) {
+      return description.replace(/<br>/g, "");
+    }
+  };
+
+  return (
+    <Popover
+      open={open}
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "left",
+      }}
+    >
+      <Box sx={{ padding: 20 }}>
+        <Typography>{item.name}</Typography>
+        <Typography>{removeLineBreaks(item.description)}</Typography>
+        <Typography>{item.price}円</Typography>
+      </Box>
+    </Popover>
+  );
+};
+
+export default ItemDetailPopup;
